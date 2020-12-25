5d8c26d96d71e2ad8f191a5a32056bd8b8d05d7b
v.eremin <v.eremin@apibank.online>
2020-12-25 18:51:24 +0300

    Initial commit

---
+++ src/main/java/ru/geekrains/hello_world/Main.java
@@ -0,0 +1,19 @@
+package ru.geekrains.hello_world;
+
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    //Основной метод
+    public static void main(String[] args) {
+        BigDecimal bigDecimal = new BigDecimal("123");
+        int[] a = {1, 2, 3};
+        int b = 5;
+        String string = "123";
+    }
+}
+
---
+++ src/main/java/ru/geekrains/hello_world/test/Test.java
@@ -0,0 +1,11 @@
+package ru.geekrains.hello_world.test;
+
+public class Test {
+    private static void method() {
+        System.out.println("Достучись до меня");
+    }
+
+    private static void test() {
+        method();
+    }
+}
